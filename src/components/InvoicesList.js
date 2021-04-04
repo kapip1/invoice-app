6190332f21dd9e539106f8f2b77e44700e8ae841
Kacper Negro <kacpernegro@icloud.com>
2021-04-04 19:59:52 +0200

    fix problem with vercel

--- src/components/InvoicesList.js
+++ src/components/InvoicesList.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-import { Paid, Pending, Draft } from '../styles/InvoiceStatus';
+import { Paid } from '../styles/InvoiceStatus';
 
 import arrow from '../assets/icon-arrow-right.svg';
 
